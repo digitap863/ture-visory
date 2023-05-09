d8e303c2aee57ffec1c83b3d19b3f6f57016105c
Asif <asifsaheer7034@gmail.com>
2023-05-10 00:43:03 +0530

    updatd

---
+++ script/value.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".slider-content-c", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-nex-c",
+      prevEl: ".swiper-button-pre-c",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 1,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+ 
