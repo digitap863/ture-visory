327a4c81771aff28cb6faa29383c33dbd5dd2522
Asif <asifsaheer7034@gmail.com>
2023-05-15 23:05:32 +0530

    updated

--- script/header.js
+++ script/header.js
@@ -5,16 +5,19 @@ btn.forEach((item) => {
         window.location.href = 'blogSingle.html'
     })
 })
+
 const navHamBurger = document.querySelector('.navHamBurger')
 const header = document.querySelector('.header')
 let navState = false
 navHamBurger.addEventListener('click', () => {
     if (!navState) {
-        header.style.height = '20rem'
+        header.style.height = '25rem'
         navState = true;
     } else {
-        header.style.height = '5rem'
+        header.style.height = '6rem'
         navState = false;
     }
 
 })
+
+
